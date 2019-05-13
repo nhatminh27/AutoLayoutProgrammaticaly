f0c31c1e6302b7e45f1b509019acbe3ef0a80a9e
Nhat Minh <minhcn@bachtuocso.vn>
2019-05-13 08:44:25 +0700

    Initial Commit

---
+++ AutoLayoutProgrammaticaly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutProgrammaticaly
+//
+//  Created by HaoLe on 5/13/19.
+//  Copyright © 2019 BTS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
